27bc11b71c0953a4b6d9801032518c544a3081ae
Nathan-Yorio <>
2023-02-15 13:37:59 +0200

    initial commit

---
+++ shell.sh
@@ -0,0 +1,6 @@
+./generate-webfonts -o assets NotoSansJP-Black.otf --css notosansjp-black.css  
+./generate-webfonts -o assets NotoSansJP-Bold.otf --css NotoSansJP-Bold.css  
+./generate-webfonts -o assets NotoSansJP-Light.otf --css NotoSansJP-Light.css  
+./generate-webfonts -o assets NotoSansJP-Medium.otf --css NotoSansJP-Medium.css  
+./generate-webfonts -o assets NotoSansJP-Regular.otf --css NotoSansJP-Regular.css  
+./generate-webfonts -o assets NotoSansJP-Thin.otf --css NotoSansJP-Thin.css  
